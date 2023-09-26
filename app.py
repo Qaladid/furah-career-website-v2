c39f03c31971b6bcc97d4b50248b8a91ee651279
Qaladid <abdiaziz11410@gmail.com>
2023-09-26 09:54:13 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, india',
+        'salary': 'Rs. 10,00,000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, india',
+        'salary': 'Rs. 15,00,000',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        
+    },
+    {
+        'id': 1,
+        'title': 'Backend Engineer',
+        'location': 'Nairobi, Kenya',
+        'salary': 'Rs. 150,00,000',
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+def hello_world():
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
